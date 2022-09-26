7b8c3502d60c0387676fd74311f26435dc11f3da
chikebaba <114248659+chikebaba@users.noreply.github.com>
2022-09-26 14:29:16 +0200

    Update SlideshowViewModel.kt

--- app/src/main/java/com/example/android_task/ui/slideshow/SlideshowViewModel.kt
+++ app/src/main/java/com/example/android_task/ui/slideshow/SlideshowViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = ""
+        value = "This is third fragment with Recycler view"
     }
     val text: LiveData<String> = _text
 }
