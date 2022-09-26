55b0738827bbe2b3b3912987383182971fde2800
chikebaba <114248659+chikebaba@users.noreply.github.com>
2022-09-26 14:27:12 +0200

    Update HomeViewModel.kt

--- app/src/main/java/com/example/android_task/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/android_task/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = ""
+        value = "This is fragment with 2 textview"
     }
     val text: LiveData<String> = _text
 }
