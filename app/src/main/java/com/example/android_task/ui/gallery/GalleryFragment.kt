763fae6133124940eddcaa4cdff70e09144e1701
chikebaba <114248659+chikebaba@users.noreply.github.com>
2022-09-26 20:19:33 +0200

    Update GalleryFragment.kt

--- app/src/main/java/com/example/android_task/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/android_task/ui/gallery/GalleryFragment.kt
@@ -16,44 +16,44 @@ import kotlinx.android.synthetic.main.fragment_gallery.*
 
 class GalleryFragment : Fragment() {
 
-private var _binding: FragmentGalleryBinding? = null
-
-  private val binding get() = _binding!!
-
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val galleryViewModel =
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val galleryViewModel =
             ViewModelProvider(this).get(GalleryViewModel::class.java)
 
-    _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-    val root: View = binding.root
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
-    val textView: TextView = binding.textGallery
-    galleryViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+        val textView: TextView = binding.textGallery
+        galleryViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
     }
-    return root
-  }
-
 
     override fun onStart() {
         super.onStart()
-   
         watchedText.addTextChangedListener(object : TextWatcher {
 
-            override fun afterTextChanged(s: Editable) {  
+            override fun afterTextChanged(s: Editable) {
             }
 
-            override fun beforeTextChanged(s: CharSequence, start: Int,
-                                           count: Int, after: Int) {
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
             }
 
-            override fun onTextChanged(s: CharSequence, start: Int,
-                                       before: Int, count: Int) {
-                Toast.makeText(context, s , Toast.LENGTH_SHORT).show()
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                Toast.makeText(context, s, Toast.LENGTH_SHORT).show()
             }
         })
     }
