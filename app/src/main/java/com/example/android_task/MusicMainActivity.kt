cee99b5fbd93e4f13894bc93e74d6afaa9413ca1
chikebaba <114248659+chikebaba@users.noreply.github.com>
2022-10-17 21:14:01 +0200

    Update MusicMainActivity.kt

--- app/src/main/java/com/example/android_task/MusicMainActivity.kt
+++ app/src/main/java/com/example/android_task/MusicMainActivity.kt
@@ -11,39 +11,41 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.android_task.databinding.ActivityMusicMainBinding
 import kotlinx.android.synthetic.main.activity_music_main.*
-
 
 class MusicMainActivity : AppCompatActivity() {
     var mMediaPlayer: MediaPlayer? = null
     private val receiver = MusicReceiver()
+    private lateinit var binding : ActivityMusicMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_music_main)
+        binding = ActivityMusicMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val intentFilter = IntentFilter("PICK_MUSIC")
         intentFilter.addAction("PICK_MUSIC")
         LocalBroadcastManager.getInstance(this).registerReceiver(receiver, intentFilter)
 
-        val button_artist = findViewById<Button>(R.id.button_artist)
+        val button_artist = binding.buttonArtist
         button_artist.setOnClickListener {
             val intent = Intent(this, MusicArtistActivity::class.java)
             startActivity(intent)
         }
 
-        val textView10: TextView = findViewById<TextView>(R.id.textView10)
-        val textView11: TextView = findViewById<TextView>(R.id.textView11)
-        val textView12: TextView = findViewById<TextView>(R.id.textView12)
+        val textViewSongName: TextView = findViewById<TextView>(R.id.textViewSongName)
+        val textViewGenre: TextView = findViewById<TextView>(R.id.textViewGenre)
+        val textViewArtist: TextView = findViewById<TextView>(R.id.textViewArtist)
 
         val songName = intent?.getStringExtra("songName")
         val songArtist = intent?.getStringExtra("songArtist")
         val songGenre = intent?.getStringExtra("songGenre")
         val songPath = intent?.getStringExtra("songPath")
 
-        textView10.setText("Name: " + songName)
-        textView11.setText("Genre: " + songGenre)
-        textView12.setText("Artist: " + songArtist)
+        textViewSongName.setText("Name: " + songName)
+        textViewGenre.setText("Genre: " + songGenre)
+        textViewArtist.setText("Artist: " + songArtist)
         button_play.setOnClickListener { playSound(songPath.toString()) }
 
     }
