99f0ad3ce6171c82ce317b481193b4aa36e4faf8
chikebaba <114248659+chikebaba@users.noreply.github.com>
2022-10-17 19:39:42 +0200

    DemoCollectionPagerAdapter to class

--- app/src/main/java/com/example/android_task/ViewPager.kt
+++ app/src/main/java/com/example/android_task/ViewPager.kt
@@ -38,15 +38,4 @@ class ViewPager : Fragment() {
     }
 }
 
-class DemoCollectionPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
 
-    override fun getItem(position: Int) = mFrgmentList[position]
-    override fun getPageTitle(position: Int) = mFrgmentTitleList[position]
-    private val mFrgmentList = ArrayList<Fragment>()
-    private val mFrgmentTitleList = ArrayList<String>()
-    override fun getCount() = mFrgmentList.size
-    fun addFragment(fragment: Fragment, title: String) {
-        mFrgmentList.add(fragment)
-        mFrgmentTitleList.add(title)
-    }
-}
