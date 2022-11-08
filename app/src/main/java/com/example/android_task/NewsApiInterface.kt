67af2fd53de9a5ce9ad97c2ccae21a777b0a4ecc
chikebaba <114248659+chikebaba@users.noreply.github.com>
2022-11-08 10:22:57 +0100

    Update NewsApiInterface.kt
    Remove api key

--- app/src/main/java/com/example/android_task/NewsApiInterface.kt
+++ app/src/main/java/com/example/android_task/NewsApiInterface.kt
@@ -6,18 +6,18 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface NewsApiInterface {
-    @GET("/v2/everything?q=tesla&from=2022-10-07&sortBy=publishedAt&apiKey=841fdcee6637468ba090bf99257ca12e")
+    @GET("/v2/everything?q=tesla&from=2022-10-07&sortBy=publishedAt&apiKey=")
     fun getNews() : Call<NewsResponse>
 
-    @GET("/v2/everything?q=apple&from=2022-11-06&to=2022-11-06&sortBy=popularity&apiKey=841fdcee6637468ba090bf99257ca12e")
+    @GET("/v2/everything?q=apple&from=2022-11-06&to=2022-11-06&sortBy=popularity&apiKey=")
     fun getNewsSoftware() : Call<NewsResponse>
 
-    @GET("/v2/top-headlines?sources=techcrunch&apiKey=841fdcee6637468ba090bf99257ca12e")
+    @GET("/v2/top-headlines?sources=techcrunch&apiKey=")
     fun getNewsScience() : Call<NewsResponse>
 
-    @GET("/v2/top-headlines?country=us&category=business&apiKey=841fdcee6637468ba090bf99257ca12e")
+    @GET("/v2/top-headlines?country=us&category=business&apiKey=")
     fun getNewsMusic() : Call<NewsResponse>
 
-    @GET("/v2/everything?domains=wsj.com&apiKey=841fdcee6637468ba090bf99257ca12e")
+    @GET("/v2/everything?domains=wsj.com&apiKey=")
     fun getNewsFilm() : Call<NewsResponse>
 }
