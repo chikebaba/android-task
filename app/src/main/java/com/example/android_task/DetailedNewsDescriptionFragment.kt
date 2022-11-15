d09ea7ae21d4bbe92bd77d41ac044f6d799c3481
chikebaba <114248659+chikebaba@users.noreply.github.com>
2022-11-15 14:36:49 +0100

    Update DetailedNewsDescriptionFragment.kt

--- app/src/main/java/com/example/android_task/DetailedNewsDescriptionFragment.kt
+++ app/src/main/java/com/example/android_task/DetailedNewsDescriptionFragment.kt
@@ -8,19 +8,7 @@ import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_detailed_news_description.*
 
-class DetailedNewsDescriptionFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detailed_news_description, container, false)
-    }
-
+class DetailedNewsDescriptionFragment : Fragment(R.layout.fragment_detailed_news_description) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val textViewNewsDetailedAuthor = textViewNewsDetailedAuthor
